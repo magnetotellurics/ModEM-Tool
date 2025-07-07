75888f2b5531585caf751ca89a0937b9245a1085
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-07-07 15:38:11 -0600

    Add documentation to readCond_3D and writeCond_3D

--- matlab/ioAscii/writeCond_3D.m
+++ matlab/ioAscii/writeCond_3D.m
@@ -1,12 +1,43 @@
 function [status] = writeCond_3D(cfile,cond,format)
+% writeCond_3D - Create a conductivity file in either WS3D or Mackie3D format
+% Usage:  [status] = writeCond_3D(cfile, cond, format)
 %
-% Usage:  [status] = writeCond_3D(cfile,cond,format)
+% This function is best used with readCond_3D.
 %
-%  writes ModelParam object, provided as structure
-%  status is total number of bytes written
+% Input Arguments
+%   cfile - File name to write too
+%     string value
+%   cond - Struct that contains information to write grid and conductivities
+%     struct value
+%       cond.paramType - Resistivity domain either LINEAR, LOGE, or LOG10
+%         string value
+%       cond.v - Conductivity values
+%         double array
+%       cond.AirCond - Value in file of air conductivity (or default: cond.paramType(1e-10))
+%         double
+%       cond.grid - Struct that describes the grid
+%         struct value
+%           grid.dx, grid.dy, grid.dz -  Grid distances in x, y and z directions in units (defualt kilometers)
+%             double array
+%           grid.nx, grid.ny - Number of grid cells in x and y direction
+%             integer value
+%           grid.NzEarth - Number of layers in the z direction (without air)
+%             integer value
+%           grid.NzAir - Number of air layers
+%             integer value
+%           grid.origin - Origin of the modeljk
+%             double array [3x1]
+%           grid.rotation - Orientation/rotation of the model
+%             double value
+%           grid.units - Model file units - default 'km'
+%             string value
+% format - Model format either: 1 = Mackie's format (default) or 2 = WS3D format 
 %
-%  format = 1 means Mackie's format (default)
-%  format = 2 means WS's format
+% Output Arguments
+%   status - Number of bytes written - Status returned by MatLab write routines
+%    integer value
+%
+% See also readCond_3D, read_mackie3d_model, read_WS3d_model, write_WS3d_model, write_mackie3d_model
 
 if nargin < 3
     format = 1;
